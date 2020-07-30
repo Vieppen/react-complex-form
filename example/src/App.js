edf94c36b9c765f40a156ad38d7453effe8e8991
ZJVieth <52859000+ZJVieth@users.noreply.github.com>
2020-07-30 16:46:43 +0200

    Initial Template Push

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'vieppen-form'
+import Form from 'vieppen-form'
 import 'vieppen-form/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Form text="Create React Library Example 😄" />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+class Form extends Component {
+  render() {
+    return <div className={styles.test}> Example Component: </div >;
+  }
+
 }
+
+
+export default Form;
